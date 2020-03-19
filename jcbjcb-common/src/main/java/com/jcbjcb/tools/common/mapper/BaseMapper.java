d100e053b5e2b1a641beb49e7006b4c25c41a9f4
jcb <jcb@jcb.com>
2020-03-19 17:11:49 +0800

    优化代码

--- jcbjcb-common/src/main/java/com/jcbjcb/tools/common/mapper/BaseMapper.java
+++ jcbjcb-common/src/main/java/com/jcbjcb/tools/common/mapper/BaseMapper.java
@@ -2,7 +2,8 @@ package com.jcbjcb.tools.common.mapper;
 
 import tk.mybatis.mapper.common.Mapper;
 import tk.mybatis.mapper.common.MySqlMapper;
+import tk.mybatis.mapper.common.special.InsertListMapper;
 
-public interface BaseMapper<T> extends Mapper<T>, MySqlMapper<T> {
+public interface BaseMapper<T> extends Mapper<T>, MySqlMapper<T>, InsertListMapper<T> {
 
 }
